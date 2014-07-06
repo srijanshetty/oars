608b9cb28a20cc4401058d02aae126ae3fe1f4fa
Srijan R Shetty <srijan.shetty@gmail.com>
2014-07-06 22:38:10 +0530

    Fixed the slow filter issue with enter

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,20 @@
-angular.module('root', ['data'])
-    .controller('index' , ['$scope', 'courseData', function($scope, courseData) {
-        $scope.value = "soume";
-        $scope.courseData = courseData;
-    }]);
+angular.module('root', ['data', 'pasvaz.bindonce'])
+    .directive('ngEnter', function () {
+        return function (scope, element, attrs) {
+            element.bind("keydown keypress", function (event) {
+                if(event.which === 13) {
+                    scope.$apply(function (){
+                        scope.$eval(attrs.ngEnter);
+                    });
 
+                    event.preventDefault();
+                }
+            });
+        };
+    })
+    .controller('index' , ['$scope', 'courseData', function($scope, courseData) {
+        $scope.courseData = courseData;
+        $scope.startSearch = function() {
+            $scope.searchQuery = $scope.search;
+        }
+    }]);
