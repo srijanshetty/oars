65aba48953ab5b75f51a04b75ea1bd62c13d4618
Srijan R Shetty <srijan.shetty@gmail.com>
2014-07-07 21:02:50 +0530

    Update 2014-07-07T15:32:50.261Z

--- README.md
+++
@@ -1,4 +0,0 @@
-An AngularJS version of OARS data
-====
-
-A pretty interface for oars data.
--- src/scripts/main.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('root', ['data', 'pasvaz.bindonce'])
-    .controller('index' , ['$scope', 'courseData', function($scope, courseData) {
-        $scope.courseData = courseData;
-    }]);
