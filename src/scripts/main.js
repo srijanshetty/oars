84254e68bdf3ef7e16eff2dd35320d4e1f1200eb
Adarsh J <root@hackmaster.in>
2014-07-07 06:56:32 +0530

    simplify angular code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,22 +1,6 @@
 'use strict';
 
 angular.module('root', ['data', 'pasvaz.bindonce'])
-    .directive('ngEnter', function () {
-        return function (scope, element, attrs) {
-            element.bind('keydown keypress', function (event) {
-                if(event.which === 13) {
-                    scope.$apply(function (){
-                        scope.$eval(attrs.ngEnter);
-                    });
-
-                    event.preventDefault();
-                }
-            });
-        };
-    })
     .controller('index' , ['$scope', 'courseData', function($scope, courseData) {
         $scope.courseData = courseData;
-        $scope.startSearch = function() {
-            $scope.searchQuery = $scope.search;
-        };
     }]);
